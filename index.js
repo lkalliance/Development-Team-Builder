507861452c97ff463697e01dca6b1a2c1e2b5358
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-02-16 12:27:28 -0600

    pseudo-code

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("I'm here!");
+init();
+
+function init() {
+    // Welcome the user
+    // call to ask for manager
+    // initiate directory array
+}
+
+function getManager(directory) {
+    // do an Inquire for manager
+    // put on directory
+    // ask if want new employee
+}
+
+function getEmployee(type, directory) {
+    // do an Inquire for employee type
+    // do an Inquire for employee stuff
+    // put on directory
+    // ask if want new employee
+}
+
+function writeHTML(directory) {
+    // build the HTML document from directory
+}
