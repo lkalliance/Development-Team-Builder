b37b73112a4004fe589834952deeef41146e0127
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-02-21 10:14:26 -0600

    Added comments to javascript

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const build = require('./lib/buildCode');
 init();
 
 function init() {
-    // Welcome the user
-    // call to ask for manager
-    // initiate directory array
+    // This function starts the whole process
 
     console.log(`Welcome!\n`);
-    console.log(`It's time to build out your team. First, let's start with a manager:\n`)
+    console.log(`It's time to build out your team. First, let's start with a manager:\n`);
+
+    // the first step: create the manager, starting with an empty roster
     create.manager([]);
 }
 
+// listen for the emitted "all done" event, and build the page
 create.finished.on('Done with prompts', (arg) => {
     fs.writeFile('./dist/index.html', build.start(arg.roster), err => {
         if(err) console.error(err);
