bb14c6fa5a8bab4685766fbcae8fca31d682a9d5
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-02-21 11:09:07 -0600

    Fine-tuned the presentation of prompts

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ init();
 function init() {
     // This function starts the whole process
 
-    console.log(`Welcome!\n`);
-    console.log(`It's time to build out your team. First, let's start with a manager:\n`);
+    console.clear();
+    console.log(`\x1b[33;1mWelcome!\x1b[0m\n`);
+    console.log(`\x1b[33mIt's time to build out your team. First, let's start with a \x1b[33;1mmanager\x1b[33m:\n`);
 
     // the first step: create the manager, starting with an empty roster
     create.manager([]);
