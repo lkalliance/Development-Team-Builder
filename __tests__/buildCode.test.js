9502f63cef425774684cde54f671d52300f6da7c
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-02-22 11:47:40 -0600

    Wrote tests of card code-building, and made bugfixes

--- __tests__/buildCode.test.js
+++ __tests__/buildCode.test.js
@@ -4,8 +4,22 @@ const engineer = require('../lib/Engineer.js');
 const intern = require('../lib/Intern.js');
 
 describe('Card building', () => {
-    it('Manager should correctly have a name, ID and email', () => {
-        expect(card(new manager("l", 22, "m", "1234567")).arguments[0].phone).toBe("1234567");
+    it('Manager card should match the given output', () => {
+        const manCard = card(new manager("name", "idNum", "mail@gmail.com", "1234567"));
+
+        expect(manCard).toBe('<div class="card"><div class="card-body"><h5 class="card-title">name<span class="icon">MGR</span></h5></div><ul class="list-group list-group-flush"><li class="list-group-item id">idNum</li><li class="list-group-item mail"><a href="mailto:mail@gmail.com">mail@gmail.com</a></li><li class="list-group-item phone">1234567</li></ul></div>');
+    });
+
+    it('Engineer card should match the given output', () => {
+        const engCard = card(new engineer("name", "idNum", "mail@gmail.com", "githubuser"));
+
+        expect(engCard).toBe('<div class="card"><div class="card-body"><h5 class="card-title">name<span class="icon">ENG</span></h5></div><ul class="list-group list-group-flush"><li class="list-group-item id">idNum</li><li class="list-group-item mail"><a href="mailto:mail@gmail.com">mail@gmail.com</a></li><li class="list-group-item code"><a href="https://www.github.com/githubuser">githubuser</a></li></ul></div>');
+    });
+
+    it('Intern card should match the given output', () => {
+        const intCard = card(new intern("name", "idNum", "mail@gmail.com", "University of Minnesota"));
+
+        expect(intCard).toBe('<div class="card"><div class="card-body"><h5 class="card-title">name<span class="icon">INT</span></h5></div><ul class="list-group list-group-flush"><li class="list-group-item id">idNum</li><li class="list-group-item mail"><a href="mailto:mail@gmail.com">mail@gmail.com</a></li><li class="list-group-item school">University of Minnesota</li></ul></div>');
     });
 
 })
